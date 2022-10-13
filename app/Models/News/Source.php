59d3dff5ce336cd96837989310fed2f3aac660e6
kesvesna <kesvesna@rambler.ru>
2022-10-13 20:25:59 +0300

    Lesson 7.6

--- app/Models/News/Source.php
+++ app/Models/News/Source.php
@@ -4,6 +4,7 @@ namespace App\Models\News;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\DB;
 
@@ -18,4 +19,8 @@ class Source extends Model
         'slug'
     ];
 
+    public function news(): HasMany
+    {
+        return $this->hasMany(News::class, 'source_id', 'id');
+    }
 }
