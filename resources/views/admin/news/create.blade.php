a78f105f443bf7312ebad91b3a542457c172f256
unknown11 <kesvesna@rambler.ru>
2022-10-24 10:34:25 +0300

    lesson 10 added ck-editor 5 options

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -73,7 +73,27 @@
     <script src="{{ asset('js/ckeditor.js') }}"></script>
     <script>
         ClassicEditor
-            .create( document.querySelector( '#description' ))
+            .create( document.querySelector( '#description' ), {
+                image: {
+                    uploadUrl: {
+                        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
+                        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
+                        filebrowserBrowserUrl: '/laravel-filemanager?type=Files',
+                        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
+                    }
+                },
+                heading: {
+                    options: [
+                        { model: 'paragraph', title: 'Параграф', class: 'ck-heading_paragraph' },
+                        { model: 'heading1', view: 'h1', title: 'Заголовок 1', class: 'ck-heading_heading1' },
+                        { model: 'heading2', view: 'h2', title: 'Заголовок 2', class: 'ck-heading_heading2' },
+                        { model: 'heading3', view: 'h3', title: 'Заголовок 3', class: 'ck-heading_heading3' },
+                        { model: 'heading4', view: 'h4', title: 'Заголовок 4', class: 'ck-heading_heading4' },
+                        { model: 'heading5', view: 'h5', title: 'Заголовок 5', class: 'ck-heading_heading5' },
+                        { model: 'heading6', view: 'h6', title: 'Заголовок 6', class: 'ck-heading_heading6' },
+                    ]
+                }
+            })
             .catch( error => {
                 console.error( error );
             } );
